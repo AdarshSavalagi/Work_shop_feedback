4dc1eb11988db3a772de716794f6974a344c4d8f
ADARSH <120472249+AdarshSavalagi@users.noreply.github.com>
2023-11-05 23:50:50 +0530

    remove venv

--- core/models.py
+++ core/models.py
@@ -1,8 +1,23 @@
+from datetime import timezone
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
-# Create your models here.
-class Students(AbstractBaseUser,PermissionsMixin):
-    name = models.CharField(max_length=50)
-    email = models.EmailField(unique=True)
-    
+from .managers import CustomUserManager
 
+
+class Students(AbstractBaseUser, PermissionsMixin):
+    name = models.CharField(max_length=30)
+    email = models.EmailField(null=True, blank=True, unique=True)
+    phone = models.CharField(max_length=20)
+    department = models.CharField(max_length=50)
+    quiz = models.CharField(max_length=100, blank=True)
+    feedback = models.CharField(max_length=100, blank=True)
+    date_joined = models.DateTimeField(auto_created=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name', ]
+
+    def __str__(self):
+        return self.usn
